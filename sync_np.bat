2baf9a4bda030e4996fb837fd71378ac6e1f09c5
michael.herzog@abbvie.com <michael.herzog@abbvie.com>
2022-04-11 21:58:50 -0400

    Regular checkin

---
+++ sync_np.bat
@@ -0,0 +1,1 @@
+aws s3 cp s3://aws-emr-resources-307112368050-us-east-2/notebooks/e-B1Q0KUEQQPVFAA751EVPPQOBR/big_data_demo/ . --exclude "*" --include "*.ipynb" --exclude "*/*" --recursive --profile personal 
